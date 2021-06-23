eda651f00c48a7778ce64d2aab011e99801a93d1
Juan Manuel comellas <jm_comellas@hotmail.com>
2021-06-23 09:42:21 -0300

    Mejora en la documentacion del codigo e inclusion de los archivos para generar la BD

--- src/main/java/com/pokedex/controllers/PokedexRestController.java
+++ src/main/java/com/pokedex/controllers/PokedexRestController.java
@@ -13,13 +13,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+//Defino el controlador REST
 @RestController
+@RequestMapping("/api")
 public class PokedexRestController {
     
 @Autowired
 private PokemonService pokemonService;
 
+/**
+ * Este mapeo del metodo GET en la direccion /pokemons retorna
+ * mediante un JSON toda la colección de pokemons contenida en
+ * la base de datos.
+ * @return Un JSON con todos los pokemons en la base de datos.
+ */
 @GetMapping(
     value="/pokemons",
 	produces = { MimeTypeUtils.APPLICATION_JSON_VALUE },
@@ -28,7 +37,12 @@ public List<Pokemon> getPokemons() {
    return pokemonService.listarPokemons();
 }
 
-
+/**
+ * Este mapeo del metodo POST en la direccion /updatePokemon 
+ * actualiza el pokemon recibido como parametro dentro de la
+ * base de datos y si no existe lo crea.
+ * @param pokemon Este parametro indica el pokemon a actualizar.
+ */
 @PostMapping(
     value="/updatePokemon",
 	produces = { MimeTypeUtils.APPLICATION_JSON_VALUE },
@@ -38,14 +52,27 @@ public void addPokemon(@RequestBody Pokemon pokemon)
     pokemonService.guardar(pokemon);
 }
 
+/**
+ * Este mapeo del metodo GET en la direccion /deletePokemon
+ * elimina el pokemon con el nombre {poke_name} recibido en
+ * forma de path variable de la BD.
+ * @param poke_name Nombre del pokemon a eliminar en la BD.
+ */
 @GetMapping(
-    value="/deletepokemon/{poke_name}",
+    value="/deletePokemon/{poke_name}",
 	produces = { MimeTypeUtils.APPLICATION_JSON_VALUE },
 	headers = "Accept=application/json")
 public void deletePokemon(@PathVariable String poke_name) {
     pokemonService.eliminar(poke_name);
 }
 
+/**
+ * Este mapeo del metodo GET en la direccion /getPokemon
+ * recupera el pokemon con el nombre {poke_name} recibido 
+ * en forma de path variable de la BD.
+ * @param poke_name Nombre del pokemon a recuperar en la BD.
+ * @return El pokemon recuperado de la base de datos.
+ */
 @GetMapping(
     value="/getPokemon/{poke_name}",
 	produces = { MimeTypeUtils.APPLICATION_JSON_VALUE },
--- src/main/java/com/pokedex/services/PokemonServiceImplementation.java
+++ src/main/java/com/pokedex/services/PokemonServiceImplementation.java
@@ -14,17 +14,32 @@ public class PokemonServiceImplementation implements PokemonService{
 
     @Autowired
     private pokemonRepository pokemons;
-
+    
+    /**
+    * Este metodo recupera todos los pokemons de la base de datos
+    * y los retorna en forma de lista. 
+    * @return Una lista que contiene todos los pokemons.
+    */
     @Transactional(readOnly = true)
     public List<Pokemon> listarPokemons() {
         return (List<Pokemon>) pokemons.findAll();
     }
 
+    /**
+     * Este metodo actualiza o guarda el pokemon recibido
+     * como parametro.
+     * @param Pokemon Este parametro indica el pokemon a guardar o
+     * actualizar dentro de la BD.
+     */
     @Transactional
     public void guardar(Pokemon pokemon) {
         pokemons.save(pokemon);
     }
-
+    /**
+     * Este metodo elimina un pokemon de la base de datos.
+     * @param String Nombre del pokemon a eliminar en la base
+     * de datos.
+     */
     @Transactional
     public void eliminar(String poke_name) {
         Pokemon aux = encontrarPokemon(poke_name);
@@ -33,6 +48,11 @@ public class PokemonServiceImplementation implements PokemonService{
         
     }
 
+    /**
+     * Este metodo recupera un pokemon de la base de datos
+     * @param String Pokemon a recuperar en la base de datos.
+     * @return Pokemon encontrado en la base de datos o null en caso contrario.
+     */
     @Transactional(readOnly = true)
     public Pokemon encontrarPokemon(String poke_name) {
         return pokemons.findById(poke_name).orElse(null);
