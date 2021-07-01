aeb4d9a756dc220e1124fc50e7914d93e50d2849
Juan Manuel comellas <jm_comellas@hotmail.com>
2021-07-01 17:30:17 -0300

    Se completó el test del controlador rest.

--- src/test/java/com/pokedex/ControllerTest.java
+++ src/test/java/com/pokedex/ControllerTest.java
@@ -4,12 +4,15 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokedex.controllers.PokedexRestController;
+import com.pokedex.data.Evolution;
 import com.pokedex.data.Pokemon;
+import com.pokedex.data.Skill;
 import com.pokedex.services.PokemonService;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -44,29 +47,55 @@ public class ControllerTest {
         pokemon_1.setName("prueba1");
         pokemon_1.setTypes("test");
         pokemon_1.setLevel(1);
+        pokemon_1.setEvolutions(new HashSet<Evolution>());
+        pokemon_1.setSkills(new HashSet<Skill>());
         pokemon_2.setName("prueba2");
         lista.add(pokemon_1);
         lista.add(pokemon_2);
 
         when(service.listarPokemons()).thenReturn(lista);
-        doNothing().when(service).eliminar("test");
+        when(service.encontrarPokemon("prueba1")).thenReturn(pokemon_1);
+        doNothing().when(service).eliminar("prueba1");
+        doNothing().when(service).guardar(pokemon_1);
 
     }
 
     @Test
     void getPokemonsTest() throws Exception
     {
-        String json = this.mockMvc.perform(get("/api/pokemons")).andDo(print()).andExpect(status().isOk())
-                         .andExpect(content().contentType("application/json"))
-                      .andReturn().getResponse().getContentAsString();
+        String json = this.mockMvc.perform(get("/api/pokemons")).andExpect(status().isOk())
+                                                                .andExpect(content().contentType("application/json"))
+                                                                .andReturn().getResponse().getContentAsString();
         List<Pokemon> pokemons = new ObjectMapper().readValue(json, new TypeReference<List<Pokemon>>(){});
         assertEquals("prueba1", pokemons.get(0).getName());
         assertEquals("prueba2", pokemons.get(1).getName());
     }
 
     @Test
-    void addPokemonTest()
+    void addPokemonTest() throws Exception
     {
-        
+        String json = "{\"name\":\"prueba1\",\"types\":\"test\",\"level\":1,\"skills\":[],\"evolutions\":[]}";
+        this.mockMvc.perform(post("/api/updatePokemon").accept("application/json")
+                                                       .content(json)
+                                                       .contentType("application/json"))
+                                                  .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletePokemonTest()throws Exception
+    {
+        this.mockMvc.perform(get("/api/deletePokemon/prueba1")).andExpect(status().isOk());
+    }
+
+    @Test
+    void getPokemonTest() throws Exception
+    {
+        String json = this.mockMvc.perform(get("/api/getPokemon/prueba1")).andExpect(status().isOk())
+                                                                          .andExpect(content().contentType("application/json"))
+                                                                          .andReturn().getResponse().getContentAsString();
+        Pokemon prueba = new ObjectMapper().readValue(json, Pokemon.class);
+        assertEquals("prueba1", prueba.getName());
+        assertEquals("test", prueba.getTypes());
+        assertEquals(1, prueba.getLevel());                                                              
     }
 }
